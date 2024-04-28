09ac84e25824165c8a9bbc9a9f191d7254fa4af6
Arthurgaa <arthurgalvaoalves22@hotmail.com>
2024-04-28 14:51:53 -0300

    Teste Validações

--- script.js
+++ script.js
@@ -51,6 +51,40 @@ form.addEventListener("submit", (event) => {
         alert("Por favor, preencha seu nome");
         return;
     }
+
+    //VERIFICA SE O E-MAIL ESTÁ VAZIO
+    if(emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha seu email");
+        return;
+    }
+
+    //VERIFICA SE SUA SENHA ESTÁ VAZIA
+    if(passwordInput === "") {
+        alert("Por favor, preencha sua senha");
+        return;
+    }
+
+    //VERIFICA SE SEU TELEFONE ESTÁ VAZIO
+    if(telInput.value === "") {
+        alert("Por favor, preencha seu telefone");
+        return;
+    }
+
+    // Se todos os campus estiverem corretamente preenchidos, envie o form
+    form.submit();
 });
 
+ //VERIFICA SE O E-MAIL É VÁLIDO
+ function isEmailValid(email){
+    // cria uma regex para validar email
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.text(email)) {
+        return true
+    }
+
+    return false;
+}
 

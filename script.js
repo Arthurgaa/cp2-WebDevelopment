6f4e4b8eacfdfa9f5ab3b7ba6a31e686cae64a07
Arthurgaa <arthurgalvaoalves22@hotmail.com>
2024-04-28 12:56:33 -0300

    Início das validações

--- script.js
+++ script.js
@@ -4,14 +4,53 @@ function validar(){
     let usuario =document.getElementById("usuario").value;
     let senha =document.getElementById("senha").value;
 
-    if (usuario ==="Admin" && senha ==="12345"){
-        window.open("quiz.html")
-    }else{
+    if (usuario ==="" && senha ===""){
         alert("usuario e senha inválidos")
+    }else{
+        window.open("quiz.html")
     }
 
 }
 
+//CRIANDO UM SLIDESHOW AUTOMÁTICO
 
+//Declarando as variaveis
+
+let imagens=[''];
+let index=0;
+let time =3000;
+
+function slideShow(){
+    document.getElementById("imgbanner").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+
+    setTimeout('slideShow()', time);
+
+}
+
+slideShow();
+
+
+//FORMULÁRIO VALIDAÇÃO DE CAMPUS VAZIO
+
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const telInput = document.querySelector("#telefone");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //VERIFICA SE O NOME ESTÁ VAZIO
+    if(nameInput.value === "") {
+        alert("Por favor, preencha seu nome");
+        return;
+    }
+});
 
 

8e126359ed24b175d724485ccec7b7e383004985
Arthurgaa <arthurgalvaoalves22@hotmail.com>
2024-04-28 22:12:37 -0300

    Arrumando os formulários

--- script.js
+++ script.js
@@ -32,45 +32,6 @@ function slideShow(){
 slideShow();
 
 
-//FORMULÁRIO VALIDAÇÃO DE CAMPUS VAZIO
-
-const form = document.querySelector("#form");
-const nameInput = document.querySelector("#name");
-const telInput = document.querySelector("#telefone");
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    //VERIFICA SE O NOME ESTÁ VAZIO
-    if(nameInput.value === "") {
-        alert("Por favor, preencha seu nome");
-        return;
-    }
-
-    //VERIFICA SE O E-MAIL ESTÁ VAZIO
-    if(emailInput.value === "" || !isEmailValid(emailInput.value)) {
-        alert("Por favor, preencha seu email");
-        return;
-    }
-
-    //VERIFICA SE SUA SENHA ESTÁ VAZIA
-    if(passwordInput === "") {
-        alert("Por favor, preencha sua senha");
-        return;
-    }
-
-    //VERIFICA SE SEU TELEFONE ESTÁ VAZIO
-    if(telInput.value === "") {
-        alert("Por favor, preencha seu telefone");
-        return;
-    }
-
-    // Se todos os campus estiverem corretamente preenchidos, envie o form
-    form.submit();
-});
-
  //VERIFICA SE O E-MAIL É VÁLIDO
  function isEmailValid(email){
     // cria uma regex para validar email

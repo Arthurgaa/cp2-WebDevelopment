ec5ea09be32732d0896760d88053ea8fb22f37b7
Arthurgaa <arthurgalvaoalves22@hotmail.com>
2024-04-29 00:10:52 -0300

    Começo do Quiz

---
+++ quiz.js
@@ -0,0 +1,8 @@
+//QUIZ BIKECITY
+let p1=prompt("Digite seu NOME")
+let p2=prompt("Digite seu PODER")
+let p3=prompt("Digite o nome de um VILÃO")
+let p4=prompt("Digite um LUGAR")
+let msg=document.getElementById("msg")
+
+msg.innerHTML=`ola ${p1}, seu poder é ${p2} e você vai enfrentar o(a) ${p3} no(em) ${p4}`
